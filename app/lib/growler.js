ab068cc09d60ff322fb5a41d8200350336cdd0fd
Dave <dave91x@gmail.com>
2014-09-27 14:11:09 -0400

    continuing work on the Neo4j mapper library code for graph databases

--- app/lib/growler.js
+++ app/lib/growler.js
@@ -2,6 +2,7 @@
 var neoDb   = require("seraph")();
 var Model   = require('./model');
 var Schema  = require('./schema');
+var pkg     = require('./package.json');
 
 /*
  * Growler constructor.
@@ -16,7 +17,7 @@ function Growler () {
   this.connections = [];
   // this.plugins = [];
   this.models = {};
-  // this.modelSchemas = {};
+  this.modelSchemas = {};
   // default global options
   this.options = {
     pluralization: true
@@ -25,10 +26,150 @@ function Growler () {
   // conn.models = this.models;
 }
 
+/*
+ * Sets growler options
+ *
+ * #### Example:
+ *
+ *     growler.set('test', value) // sets the 'test' option to `value`
+ *
+ *     growler.set('debug', true) // enable logging collection methods + arguments to the console
+ *
+ * @param {String} key
+ * @param {String} value
+ * @api public
+ */
 
-Growler.prototype.Model  = Model;
+Growler.prototype.set = function (key, value) {
+  if (arguments.length == 1) {
+    return this.options[key];
+  }
+
+  this.options[key] = value;
+  return this;
+};
+
+/*
+ * Gets growler options
+ *
+ * #### Example:
+ *
+ *     growler.get('test') // returns the 'test' value
+ *
+ * @param {String} key
+ * @method get
+ * @api public
+ */
+
+Growler.prototype.get = Growler.prototype.set;
+
+/*
+ * Defines a model or retrieves it.
+ *
+ * Models defined on the `growler` instance are available to all connection created by the same `growler` instance.
+ *
+ * #### Example:
+ *
+ *     var growler = require('growler');
+ *
+ *     // define an Actor model with this growler instance
+ *     growler.model('Actor', new Schema({ name: String }));
+ *
+ *     // create a new connection
+ *     var conn = growler.createConnection(..);
+ *
+ *     // retrieve the Actor model
+ *     var Actor = conn.model('Actor');
+ *
+ * _When no `collection` argument is passed, Growler produces a collection name by passing the model `name`
+ *  to the [utils.toCollectionName](#utils_exports.toCollectionName) method. This method pluralizes the name. 
+ *  If you don't like this behavior, either pass a collection name or set your schemas collection name option._
+ *
+ * #### Example:
+ *
+ *     var schema = new Schema({ name: String }, { collection: 'actor' });
+ *
+ *     // or
+ *
+ *     schema.set('collection', 'actor');
+ *
+ *     // or
+ *
+ *     var collectionName = 'actor'
+ *     var M = growler.model('Actor', schema, collectionName)
+ *
+ * @param {String} name model name
+ * @param {Schema} [schema]
+ * @param {String} [collection] name (optional, induced from model name)
+ * @param {Boolean} [skipInit] whether to skip initialization (defaults to false)
+ * @api public
+ */
+ 
+Growler.prototype.model  = function (name, schema) {
+  
+  return this.models[name] = model;
+};
+
+/*
+ * Returns an array of model names created on this instance of Mongoose.
+ *
+ * #### Note:
+ *
+ * _Does not include names of models created using `connection.model()`._
+ *
+ * @api public
+ * @return {Array}
+ */
+
+Mongoose.prototype.modelNames = function () {
+  var names = Object.keys(this.models);
+  return names;
+}
+
+/*
+ * The Mongoose version
+ *
+ * @property version
+ * @api public
+ */
+
+Mongoose.prototype.version = pkg.version;
+
+/*
+ * The Mongoose constructor
+ *
+ * The exports of the mongoose module is an instance of this class.
+ *
+ * #### Example:
+ *
+ *     var mongoose = require('mongoose');
+ *     var mongoose2 = new mongoose.Mongoose();
+ *
+ * @method Mongoose
+ * @api public
+ */
+
+Mongoose.prototype.Mongoose = Mongoose;
+
+/*
+ * The Mongoose [Schema](#schema_Schema) constructor
+ *
+ * #### Example:
+ *
+ *     var mongoose = require('mongoose');
+ *     var Schema = mongoose.Schema;
+ *     var CatSchema = new Schema(..);
+ *
+ * @method Schema
+ * @api public
+ */
 
 Growler.prototype.Schema = Schema;
 
-
+/*!
+ * The exports object is an instance of Mongoose.
+ *
+ * @api public
+ */
+ 
 var growler = module.exports = exports = new Growler;
--- app/models/user.js
+++ app/models/user.js
@@ -1,20 +1,18 @@
 // user model declaration
 
 // eventually tie in growler db adapter here
-var growler = require('../lib/growler');
-
-console.log(growler.Model('sam'));
-
-
-var _ = require('underscore');
+var growler    = require('../lib/growler');
+var bcrypt     = require('bcrypt-nodejs');
+var _          = require('underscore');
+// console.log(growler.model('sam'));
 
 var users = {
 
   data: [ {uid: 1, name: 'Dan'},
           {uid: 2, name: 'Dave'},
-          {uid: 3, name: 'Amy'},
+          {uid: 3, name: 'Renee'},
           {uid: 4, name: 'Sarah'},
-          {uid: 5, name: 'Miranda'},
+          {uid: 5, name: 'Moby'},
           {uid: 6, name: 'Roger'},
           {uid: 7, name: 'Tracey'},
           {uid: 8, name: 'Cindy'},
